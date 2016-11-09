3df5d7d868dd67aa4304065a587a102cd68df184
ivo <ursinoivo@gmail.com>
2016-11-09 00:39:57 -0300

    Elipse rotada

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-
 function main (ctx, canvas) {
 
-  canvas.width = 1280
-  canvas.height = 720
+  canvas.width = 1920
+  canvas.height = 1080
 
-  var colors = ['yellow', 'red', 'green', 'blue', 'purple', 'black', 'pink', 'grey', 'gold', 'brown']
-  var squareSize = 20;
+	centro_x = 960;
+	centro_y = 540;
 
-  for(var j = 0; j<1280; j+=squareSize)
-  {
-	  for(var i = 0 + j; i<1280; i+=squareSize)
-	  {
-		ctx.fillStyle = getRandomColor()
-	  	ctx.fillRect(i, j, squareSize, squareSize)
+	ctx.fillStyle = "#4CFF90";
+	ctx.globalAlpha = 0.7;
+	ctx.fillRect( 0, 0, canvas.width, canvas.height);
+	
+  radio_y = 200000;
+  rotation = 30 * Math.PI/180;
+  lineWidth = 1;
 
-	  	ctx.fillStyle = getRandomColor()
-	  	ctx.fillRect(j, i, squareSize, squareSize)
+  globalAlpha = 1;
 
-	  	i+=squareSize;
+  //void ctx.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise);
 
-	  	ctx.fillStyle = getRandomColor()
-	  	ctx.fillRect(i, j, squareSize, squareSize)  	
+  for(var i = 0; i <= 700; i ++)
+	{
+    radio_y = radio_y - radio_y * 0.02;
+    rotation = rotation + 7 * Math.PI / 180;
+    lineWidth = lineWidth - lineWidth * 0.002; 
+    globalAlpha = globalAlpha - 0.0015;
 
-	  	ctx.fillStyle = getRandomColor()
-	  	ctx.fillRect(j, i, squareSize, squareSize)
-	  }  	
+    ctx.beginPath();
+    ctx.ellipse(centro_x, centro_y, 150, radio_y, rotation, 150, 359);
+    ctx.strokeStyle = getRandomColor();
+		ctx.globalAlpha = globalAlpha;
+    ctx.stroke();
+  }
+
+  radio_y = 200000;
+  rotation = 120 * Math.PI/180;
+  lineWidth = 1;
+  globalAlpha = 1;
+
+  for(var j = 0; j <= 700; j ++)
+	{
+    radio_y = radio_y - radio_y * 0.02;
+    rotation = rotation + 7 * Math.PI / 180;
+    lineWidth = lineWidth - lineWidth * 0.002; 
+    globalAlpha = globalAlpha - 0.0015;
+
+    ctx.beginPath();
+    ctx.globalAlpha = globalAlpha;
+    ctx.ellipse(centro_x, centro_y, 150, radio_y, rotation, 150, 359);
+
+    ctx.strokeStyle = getRandomColor();
+		ctx.globalAlpha = globalAlpha;
+
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
   }
 
 }
 
 function getRandomColor() {
+/*
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
     }
-    return color;
+*/
+		colores = ["#007F30","#267F48","#00CC4D"];
+		
+    return colores[Math.floor(Math.random() * 3)];
 }
